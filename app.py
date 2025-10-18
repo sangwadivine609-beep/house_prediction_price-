6b3d35eea2f52cec8f8f52837368c262965ddbfe
sangwadivine609-beep <sangwadivine609@gmail.com>
2025-10-18 21:56:00 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 17 09:48:29 2025
+
+@author: user
+"""
+
+import numpy as np
+import streamlit as st
+import pickle
+
+# Load the trained model
+with open('house_sales_data.sav', 'rb') as file:
+    model = pickle.load(file)
+
+# Streamlit UI
+st.title("🏠 House Price Regression Prediction")
+st.write("Fill in the details below to predict the estimated house price:")
+
+# User Inputs
+area = st.number_input("area", value=0.0)
+rooms = st.number_input("rooms", value=0.0)
+
+
+
+# Prediction on button click
+if st.button("Predict"):
+    # Create input in correct order and shape
+    input_data = np.array([[area,rooms]])
+
+    # Predict using loaded model
+    prediction = model.predict(input_data)
+
+    # Show result
+    st.success(f"💰 Predicted House Price: ${prediction[0]:,.2f}")
